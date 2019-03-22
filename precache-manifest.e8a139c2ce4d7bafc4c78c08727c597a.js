e86ed240f218869d0fd3b7ce0081bbf06605ef55
Sebastian <31919808+Scherlock90@users.noreply.github.com>
2019-03-22 23:34:46 +0100

    Updates

---
+++ precache-manifest.e8a139c2ce4d7bafc4c78c08727c597a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "40e2f762dba447ece381",
+    "url": "/static/js/main.4ae0eebf.chunk.js"
+  },
+  {
+    "revision": "2c2df48eb75cbd218432",
+    "url": "/static/js/2.ebf4fc0f.chunk.js"
+  },
+  {
+    "revision": "40e2f762dba447ece381",
+    "url": "/static/css/main.ef8546bb.chunk.css"
+  },
+  {
+    "revision": "c12891e59c4670c111a31a59ca80c063",
+    "url": "/index.html"
+  }
+];
