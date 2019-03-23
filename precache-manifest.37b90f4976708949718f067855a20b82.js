cdec2c1d40cf805c171a45bf9667d2ac091abe72
Sebastian <31919808+Scherlock90@users.noreply.github.com>
2019-03-23 07:44:56 +0100

    Updates

---
+++ precache-manifest.37b90f4976708949718f067855a20b82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ReactMOSPage/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "030d1b02b4b1edeb4963",
+    "url": "/ReactMOSPage/static/js/runtime~main.b1af2b76.js"
+  },
+  {
+    "revision": "edef7fc930c64d770d69",
+    "url": "/ReactMOSPage/static/js/main.fb1c46de.chunk.js"
+  },
+  {
+    "revision": "abe91ec090c5f2693eda",
+    "url": "/ReactMOSPage/static/js/2.d359ce29.chunk.js"
+  },
+  {
+    "revision": "edef7fc930c64d770d69",
+    "url": "/ReactMOSPage/static/css/main.7ee6d5f9.chunk.css"
+  },
+  {
+    "revision": "7b54e3efb55d3011a6ec6a40c7002c81",
+    "url": "/ReactMOSPage/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e5cd7067ebd09731b1566386c894ce65.js"
+  "/ReactMOSPage/precache-manifest.37b90f4976708949718f067855a20b82.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ReactMOSPage/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
